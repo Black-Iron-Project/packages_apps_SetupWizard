baikalos/android_packages_apps_SetupWizard

SetupWizard: Handle navbar and hw keys
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/org/lineageos/setupwizard/LineageSettingsActivity.java
@@ -21,6 +21,8 @@ import android.widget.TextView;
 import lineageos.hardware.LineageHardwareManager;
 import lineageos.providers.LineageSettings;
 
+import com.android.internal.util.crdroid.Utils;
+
 public class LineageSettingsActivity extends BaseSetupWizardActivity {
 
     private SetupWizardApp mSetupWizardApp;
@@ -76,7 +78,8 @@ public class LineageSettingsActivity extends BaseSetupWizardActivity {
         mSupportsKeyDisabler = isKeyDisablerSupported(this);
         if (mSupportsKeyDisabler) {
             mNavKeys.setChecked(LineageSettings.System.getIntForUser(getContentResolver(),
-                    LineageSettings.System.FORCE_SHOW_NAVBAR, 0, UserHandle.USER_CURRENT) != 0);
+                    LineageSettings.System.FORCE_SHOW_NAVBAR,
+                    Utils.hasNavbarByDefault(this) ? 1 : 0, UserHandle.USER_CURRENT) != 0);
         } else {
             navKeysRow.setVisibility(View.GONE);
         }
@@ -117,7 +120,8 @@ public class LineageSettingsActivity extends BaseSetupWizardActivity {
         if (mSupportsKeyDisabler) {
             final Bundle myPageBundle = mSetupWizardApp.getSettingsBundle();
             boolean enabled = LineageSettings.System.getIntForUser(getContentResolver(),
-                    LineageSettings.System.FORCE_SHOW_NAVBAR, 0, UserHandle.USER_CURRENT) != 0;
+                    LineageSettings.System.FORCE_SHOW_NAVBAR,
+                    Utils.hasNavbarByDefault(this) ? 1 : 0, UserHandle.USER_CURRENT) != 0;
             boolean checked = myPageBundle.containsKey(DISABLE_NAV_KEYS) ?
                     myPageBundle.getBoolean(DISABLE_NAV_KEYS) :
                     enabled;

src/org/lineageos/setupwizard/util/SetupWizardUtils.java
@@ -50,6 +50,8 @@ import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
+import com.android.internal.util.crdroid.Utils;
+
 import lineageos.hardware.LineageHardwareManager;
 import lineageos.providers.LineageSettings;
 
@@ -315,11 +317,14 @@ public class SetupWizardUtils {
 
     private static void writeDisableNavkeysOption(Context context, boolean enabled) {
         final boolean virtualKeysEnabled = LineageSettings.System.getIntForUser(
-                context.getContentResolver(), LineageSettings.System.FORCE_SHOW_NAVBAR, 0,
-                UserHandle.USER_CURRENT) != 0;
+                context.getContentResolver(), LineageSettings.System.FORCE_SHOW_NAVBAR,
+                Utils.hasNavbarByDefault(context) ? 1 : 0, UserHandle.USER_CURRENT) != 0;
         if (enabled != virtualKeysEnabled) {
             LineageSettings.System.putIntForUser(context.getContentResolver(),
                     LineageSettings.System.FORCE_SHOW_NAVBAR, enabled ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+            Settings.System.putIntForUser(context.getContentResolver(),
+                    Settings.System.HARDWARE_KEYS_DISABLE, enabled ? 1 : 0,
                     UserHandle.USER_CURRENT);
 
             final LineageHardwareManager hardware = LineageHardwareManager.getInstance(context);

Black-Iron-Project/packages_apps_SetupWizard

src/org/lineageos/setupwizard/LineageSettingsActivity.java
@@ -21,6 +21,8 @@ import android.widget.TextView;
 import lineageos.hardware.LineageHardwareManager;
 import lineageos.providers.LineageSettings;
 
+import com.android.internal.util.blackiron.Utils;
+
 public class LineageSettingsActivity extends BaseSetupWizardActivity {
 
     private SetupWizardApp mSetupWizardApp;
@@ -76,7 +78,8 @@ public class LineageSettingsActivity extends BaseSetupWizardActivity {
         mSupportsKeyDisabler = isKeyDisablerSupported(this);
         if (mSupportsKeyDisabler) {
             mNavKeys.setChecked(LineageSettings.System.getIntForUser(getContentResolver(),
-                    LineageSettings.System.FORCE_SHOW_NAVBAR, 0, UserHandle.USER_CURRENT) != 0);
+                    LineageSettings.System.FORCE_SHOW_NAVBAR,
+                    Utils.hasNavbarByDefault(this) ? 1 : 0, UserHandle.USER_CURRENT) != 0);
         } else {
             navKeysRow.setVisibility(View.GONE);
         }
@@ -117,7 +120,8 @@ public class LineageSettingsActivity extends BaseSetupWizardActivity {
         if (mSupportsKeyDisabler) {
             final Bundle myPageBundle = mSetupWizardApp.getSettingsBundle();
             boolean enabled = LineageSettings.System.getIntForUser(getContentResolver(),
-                    LineageSettings.System.FORCE_SHOW_NAVBAR, 0, UserHandle.USER_CURRENT) != 0;
+                    LineageSettings.System.FORCE_SHOW_NAVBAR,
+                    Utils.hasNavbarByDefault(this) ? 1 : 0, UserHandle.USER_CURRENT) != 0;
             boolean checked = myPageBundle.containsKey(DISABLE_NAV_KEYS) ?
                     myPageBundle.getBoolean(DISABLE_NAV_KEYS) :
                     enabled;

src/org/lineageos/setupwizard/util/SetupWizardUtils.java
@@ -50,6 +50,8 @@ import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
+import com.android.internal.util.blackiron.Utils;
+
 import lineageos.hardware.LineageHardwareManager;
 import lineageos.providers.LineageSettings;
 
@@ -315,11 +317,14 @@ public class SetupWizardUtils {
 
     private static void writeDisableNavkeysOption(Context context, boolean enabled) {
         final boolean virtualKeysEnabled = LineageSettings.System.getIntForUser(
-                context.getContentResolver(), LineageSettings.System.FORCE_SHOW_NAVBAR, 0,
-                UserHandle.USER_CURRENT) != 0;
+                context.getContentResolver(), LineageSettings.System.FORCE_SHOW_NAVBAR,
+                Utils.hasNavbarByDefault(context) ? 1 : 0, UserHandle.USER_CURRENT) != 0;
         if (enabled != virtualKeysEnabled) {
             LineageSettings.System.putIntForUser(context.getContentResolver(),
                     LineageSettings.System.FORCE_SHOW_NAVBAR, enabled ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+            Settings.System.putIntForUser(context.getContentResolver(),
+                    Settings.System.HARDWARE_KEYS_DISABLE, enabled ? 1 : 0,
                     UserHandle.USER_CURRENT);
 
             final LineageHardwareManager hardware = LineageHardwareManager.getInstance(context);